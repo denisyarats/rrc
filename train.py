588e67ec80a5ffabddeaac386decc1af84c557c8
davidbrandfonbrener <david.brandfonbrener@gmail.com>
2020-08-22 17:56:25 -0500

    added pick curriculum

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ class Workspace(object):
         self.eval_env = envs.make(cfg.env, cfg.action_type, cfg.action_repeat,
                                   cfg.episode_length, self.eval_initializer,
                                   cfg.seed + 1,
-                                  cfg.use_curriculum, cfg.start_shape,
+                                  False, cfg.start_shape,
                                   cfg.goal_shape, cfg.curriculum_buffer_capacity,
                                   cfg.R_min, cfg.R_max, cfg.new_goal_freq,
                                   cfg.target_task_freq, cfg.n_random_actions)
