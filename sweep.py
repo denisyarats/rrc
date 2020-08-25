736f7c9717f5b20bc617dd863f7481a3d3ea6752
davidbrandfonbrener <david.brandfonbrener@gmail.com>
2020-08-25 16:33:04 -0500

    sweep

--- sweep.py
+++ sweep.py
@@ -58,9 +58,13 @@ def main():
     overrides.add(key='train_initializer', values=['fixed_goal'])
     overrides.add(key='eval_initializer', values=['fixed_goal'])
 <<<<<<< HEAD
+<<<<<<< HEAD
     overrides.add(key='lr', values=[1e-3,1e-4,1e-5])
 =======
     overrides.add(key='lr', values=[1e-5])
+>>>>>>> sweep
+=======
+    overrides.add(key='lr', values=[1e-3,1e-4,1e-5])
 >>>>>>> sweep
     overrides.add(key='batch_size', values=[128])
     overrides.add(key='actor_stddev', values=[0.2])
