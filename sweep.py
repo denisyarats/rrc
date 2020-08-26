ece1658143ae48282989e5ab4ca1255cdaec4d8b
davidbrandfonbrener <david.brandfonbrener@gmail.com>
2020-08-26 08:48:49 -0500

    m=1

--- sweep.py
+++ sweep.py
@@ -54,10 +54,10 @@ def main():
     overrides.add(key='save_frequency', values=[50000])
 
     overrides.add(key='action_type', values=['torque'])
-    overrides.add(key='episode_length', values=[250])
+    overrides.add(key='episode_length', values=[250,500,1000])
     overrides.add(key='train_initializer', values=['fixed_goal'])
     overrides.add(key='eval_initializer', values=['fixed_goal'])
-    overrides.add(key='lr', values=[1e-3,1e-4,1e-5])
+    overrides.add(key='lr', values=[1e-4])
     overrides.add(key='batch_size', values=[128])
     overrides.add(key='actor_stddev', values=[0.2])
     overrides.add(key='nstep', values=[5])
